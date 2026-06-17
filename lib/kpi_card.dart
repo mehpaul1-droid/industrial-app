15f4f8627a5fc98c6fef4e8c8a7a542ab936feb7
mehpaul1-droid <mehpaul.1@gmail.com>
2026-06-17 04:50:49 +0330

    Create kpi_card.dart

---
+++ lib/kpi_card.dart
@@ -0,0 +1,38 @@
+import 'package:flutter/material.dart';
+
+class KPICard extends StatelessWidget {
+  final String title;
+  final String value;
+  final IconData icon;
+
+  const KPICard({
+    super.key,
+    required this.title,
+    required this.value,
+    required this.icon,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      child: Padding(
+        padding: const EdgeInsets.all(16),
+        child: Column(
+          children: [
+            Icon(icon, size: 30),
+            const SizedBox(height: 10),
+            Text(title),
+            const SizedBox(height: 5),
+            Text(
+              value,
+              style: const TextStyle(
+                fontSize: 18,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
