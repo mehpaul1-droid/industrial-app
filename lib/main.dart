b68e10427f3641b69de8934277d61612bf7c30f0
mehpaul1-droid <mehpaul.1@gmail.com>
2026-06-17 07:08:17 +0330

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,21 +1,22 @@
 import 'package:flutter/material.dart';
+import 'screens/dashboard.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(const FarmAIApp());
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+class FarmAIApp extends StatelessWidget {
+  const FarmAIApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: Scaffold(
-        body: Center(
-          child: Text('App is working'),
-        ),
+      title: 'Industrial Farm AI',
+      theme: ThemeData(
+        useMaterial3: true,
       ),
+      home: const Dashboard(),
     );
   }
 }
