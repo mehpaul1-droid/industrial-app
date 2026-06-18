dd03a7a70ac2f1f5650ff7d7f3f688762222fbe9
mehpaul1-droid <mehpaul.1@gmail.com>
2026-06-18 04:04:03 +0330

    Update farm_model.dart

--- lib/models/farm_model.dart
+++ lib/models/farm_model.dart
@@ -1,25 +1,49 @@
 class FarmModel {
+  final int? id;
+  final String name;
+
   final String animal;
   final int count;
   final int age;
   final double weight;
   final String goal;
 
+  final DateTime createdAt;
+
   FarmModel({
+    this.id,
+    required this.name,
     required this.animal,
     required this.count,
     required this.age,
     required this.weight,
     required this.goal,
+    required this.createdAt,
   });
 
-  Map<String, dynamic> toJson() {
+  Map<String, dynamic> toMap() {
     return {
+      "id": id,
+      "name": name,
       "animal": animal,
       "count": count,
       "age": age,
       "weight": weight,
       "goal": goal,
+      "createdAt": createdAt.toIso8601String(),
     };
+  }
+
+  factory FarmModel.fromMap(Map<String, dynamic> map) {
+    return FarmModel(
+      id: map["id"],
+      name: map["name"],
+      animal: map["animal"],
+      count: map["count"],
+      age: map["age"],
+      weight: map["weight"],
+      goal: map["goal"],
+      createdAt: DateTime.parse(map["createdAt"]),
+    );
   }
 }
